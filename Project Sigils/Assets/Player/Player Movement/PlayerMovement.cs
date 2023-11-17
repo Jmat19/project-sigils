da7021815013e883d9086d6047e71e92a97e0083
Jmat19 <47995875+Jmat19@users.noreply.github.com>
2023-11-16 19:26:28 -0500

    Commit the fourth

--- Project Sigils/Assets/Player/Player Movement/PlayerMovement.cs
+++ Project Sigils/Assets/Player/Player Movement/PlayerMovement.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    public float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
